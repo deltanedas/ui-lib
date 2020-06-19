47a4c8a6db84695c696b8a093867f816e2815984
DeltaNedas <deltanedas@gmail.com>
2020-06-19 10:49:11 +0100

    1.7.2: refactor

--- scripts/library.js
+++ scripts/library.js
@@ -62,6 +62,50 @@ ui.once = (loading, loaded) => {
 	return true;
 };
 
+/* Run events to add UI and stuff when assets are ready. */
+ui.load = () => {
+	var table;
+	for (var i in ui.areas) {
+		table = new Table();
+		table.setFillParent(true);
+		table.visible(boolp(() => Vars.ui.hudfrag.shown()));
+		ui.areas[i].table = table;
+		ui.areas[i].init(table);
+	}
+
+	const events = ui.loadEvents;
+	ui.loaded = true;
+	for (var i in events) {
+		events[i]();
+	}
+	// Don't call events multiple times when mods reload
+	ui.loadEvents = [];
+
+	var area;
+	for (var i in ui.areas) {
+		area = ui.areas[i];
+		// Sort the cells by name
+		area.table.cells.sortComparing(func(cell => cell.get().name));
+
+		area.post(area.table);
+		// Add the UI elements to the HUD
+		Vars.ui.hudGroup.addChild(area.table);
+	}
+};
+
+/* Clear any old elements when reloading mods.
+	Call ui.load afterwards. */
+ui.clear = () => {
+	var area;
+	for (var i in ui.areas) {
+		area = ui.areas[i];
+		area.table.clearChildren();
+		if (area.reloaded) area.reloaded();
+	}
+
+	ui.effects = [];
+};
+
 ui.getIcon = (icon) => {
 	// () => Icon.leftSmall
 	if (typeof(icon) == "function") {
--- scripts/main.js
+++ scripts/main.js
@@ -14,58 +14,13 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-require("ui-lib/library");
-
+var ui = require("ui-lib/library");
 require("areas");
-
-var ui = this.global.uiLib;
-
 require("effects");
 
-// Run events to add UI and stuff when assets are ready
-function loaded() {
-	var table;
-	for (var i in ui.areas) {
-		table = new Table();
-		table.setFillParent(true);
-		table.visible(boolp(() => Vars.ui.hudfrag.shown()));
-		ui.areas[i].table = table;
-		ui.areas[i].init(table);
-	}
-
-	const events = ui.loadEvents;
-	ui.loaded = true;
-	for (var i in events) {
-		events[i]();
-	}
-	// Don't call events multiple times when mods reload
-	ui.loadEvents = [];
-
-	var area;
-	for (var i in ui.areas) {
-		area = ui.areas[i];
-		// Sort the cells by name
-		area.table.cells.sortComparing(func(cell => cell.get().name));
-
-		area.post(area.table);
-		// Add the UI elements to the HUD
-		Vars.ui.hudGroup.addChild(area.table);
-	}
-}
-
-// TODO: find a proper way to run a function once
+// TODO: find a proper way to run a function once, this breaks when installing/enabling ui lib for the first time
 ui.once(null, () => {
 	// Only hook the reload event once
-	Events.on(EventType.ContentReloadEvent, run(() => {
-		// Clear any old elements when reloading
-		var area;
-		for (var i in ui.areas) {
-			area = ui.areas[i];
-			area.table.clearChildren();
-			if (area.reloaded) area.reloaded();
-		}
-
-		ui.effects = [];
-	}));
-	loaded()
+	Events.on(EventType.ContentReloadEvent, run(ui.clear));
+	ui.load();
 });
