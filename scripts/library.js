7bbbabc90caef4d53f619b7c7f5ba102629699e7
DeltaNedas <deltanedas@gmail.com>
2020-09-13 23:47:06 +0100

    2.15.7: use user func in addMenuButton

--- scripts/library.js
+++ scripts/library.js
@@ -181,6 +181,7 @@ ui.addButton = (name, icon, clicked, user) => {
 ui.addMenuButton = (name, icon, clicked, user) => {
 	ui.addTable("menu", name, t => {
 		t.button(name, ui.getIcon(icon), clicked).height(48).size(210, 84);
+		if (user) user(t);
 	});
 };
 
